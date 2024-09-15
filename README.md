fa86c3e8c65ce6b4bdeba635e094e19d5c2fa8a9
EnriqueAtNewMachina <136668409+enriquenewmachina@users.noreply.github.com>
2024-09-14 22:13:38 -0500

    First time checkin
    First time checkin

---
+++ README.md
@@ -0,0 +1,7 @@
+# video_001451
+# 
+Contents:
+Pinecode Python examples showing Creating Index
+
+Dependency:
+pip3 install pinecone-client
---
+++ main/exampleCreateIndex.py
@@ -0,0 +1,23 @@
+import time
+from pinecone import Pinecone, ServerlessSpec
+
+pc = Pinecone(
+    api_key="YOUR-API-KEY"
+)
+
+# Create Index if not already created
+if 'my-index' not in pc.list_indexes().names():
+     pc.create_index(
+        name='my-index', 
+        dimension=1536, 
+        metric='euclidean',
+        spec=ServerlessSpec(
+            cloud='aws',
+            region='us-east-1'
+        )
+    )
+     
+while not pc.describe_index('my-index').index.status['ready']:
+    time.sleep(1)
+    
+print("Pinecone Index provisioned")
